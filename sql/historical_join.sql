b6c9a2d5f4da6da09ca6808a9c19ffc19203e1cf
sadhvika-polapelly <sadhvikapolapelly120@gmail.com>
2026-02-25 10:27:28 +0000

    update

--- sql/historical_join.sql
+++ sql/historical_join.sql
@@ -1,3 +1,4 @@
+-- Historical Join
 USE WAREHOUSE COMPUTE_WH;
 USE DATABASE SNOWFLAKE_LEARNING_DB;
 
@@ -11,3 +12,42 @@ FROM SILVER.STORE_SALES_CLEAN ss
 JOIN SNOWFLAKE_SAMPLE_DATA.TPCDS_SF100TCL.DATE_DIM d
     ON ss.ss_sold_date_sk = d.d_date_sk;
 
+
+-- ACtual historical join
+SELECT
+    f.transaction_date,
+    d.state,
+    SUM(f.ss_sales_price) AS total_sales
+FROM SILVER.STORE_SALES_ENRICHED f
+JOIN SILVER.CUSTOMER_DIM d
+    ON f.ss_customer_sk = d.customer_id
+    AND f.transaction_date >= d.start_date
+    AND (f.transaction_date < d.end_date OR d.end_date IS NULL)
+GROUP BY
+    f.transaction_date,
+    d.state
+ORDER BY
+    f.transaction_date;
+
+
+-- simulate the incremental fact load
+CREATE OR REPLACE TABLE SILVER.FACT_STAGE (
+    customer_id NUMBER,
+    store_id NUMBER,
+    sales_price NUMBER,
+    transaction_date DATE
+);
+
+-- Load the facts with surrogate keys
+
+CREATE OR REPLACE TABLE SILVER.FACT_SALES AS
+SELECT
+    d.customer_sk,
+    f.store_id,
+    f.sales_price,
+    f.transaction_date
+FROM SILVER.FACT_STAGE f
+JOIN SILVER.CUSTOMER_DIM d
+    ON f.customer_id = d.customer_id
+    AND f.transaction_date >= d.start_date
+    AND (f.transaction_date < d.end_date OR d.end_date IS NULL);
